1a4b67965aa51e1fbffd046ce24d4fdbc48f5507
Jonathan Torre <49402928+angelxtrme@users.noreply.github.com>
2025-03-31 13:53:23 +0800

    2

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ serve(async (_req: Request) => {
   if (!_req.url.endsWith("favicon.ico")) {
     const { searchParams, pathname } = new URL(_req.url);
   
-		const type = pathname.split('/')[1];
-		const field = pathname.split('/')[2];
-		const key = (type && field) ? type+':'+field : null;
+    const type = pathname.split('/')[1];
+    const field = pathname.split('/')[2];
+    const key = (type && field) ? type+':'+field : null;
 
     const search = searchParams.get('search');
     const last_name = searchParams.get('last_name');
@@ -15,8 +15,8 @@ serve(async (_req: Request) => {
     const employee_id = searchParams.get('employee_id');
     const biometrics_no = searchParams.get('biometrics_no');
 
-		console.log('[Start] '+key);
-		console.time('Duration');
+    console.log('[Start] '+key);
+    console.time('Duration');
 
     // ! ----------------------------------------------------------------------------------------------------
 
@@ -27,60 +27,63 @@ serve(async (_req: Request) => {
 
     // ! ----------------------------------------------------------------------------------------------------
 
-		if(!await redis.exists(key)) {
-			console.timeEnd('Duration');
-			console.log('[End] '+key);
+    if(!await redis.exists(key)) {
+      console.timeEnd('Duration');
+      console.log('[End] '+key);
 
       return new Response(JSON.stringify({
         [key]: null
       }), {
-        status: 400
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json'
+        }
       });
-		}
+    }
 
-		// ! ----------------------------------------------------------------------------------------------------
+    // ! ----------------------------------------------------------------------------------------------------
 
-		let data = null;
+    let data = null;
 
-		if(key === 'json:employees') {
-			if(search) {
-				const regExp = new RegExp(search, 'i');
-				const result = await redis.json.get(key);
+    if(key === 'json:employees') {
+      if(search) {
+        const regExp = new RegExp(search, 'i');
+        const result = await redis.json.get(key);
 
-				data = result.length
-					? result.reduce((accumulator, currentEmployee) => {
-						const { lastName, firstName, employeeID, biometricsNo } = currentEmployee;
+        data = result.length
+          ? result.reduce((accumulator, currentEmployee) => {
+	    const { lastName, firstName, employeeID, biometricsNo } = currentEmployee;
 
-						if(regExp.test(lastName) === true || regExp.test(firstName) === true
-							|| biometricsNo === search || employeeID === search) {
-							accumulator = [
-								...accumulator,
-								currentEmployee
-							];
-						}
+	    if(regExp.test(lastName) === true || regExp.test(firstName) === true
+	      || biometricsNo === search || employeeID === search) {
+	      accumulator = [
+	        ...accumulator,
+		currentEmployee
+	      ];
+	    }
 
-						return accumulator;
-					}, [])
-					: [];
-			} else if(last_name) {
-				data = await redis.json.get(key, '$.[?(@.lastName == "'+last_name+'")]');
-			} else if(first_name) {
-				data = await redis.json.get(key, '$.[?(@.firstName == "'+first_name+'")]');
-			} else if(employee_id) {
-				data = await redis.json.get(key, '$.[?(@.employeeID == "'+employee_id+'")]');
-			} else if(biometrics_no) {
-				data = await redis.json.get(key, '$.[?(@.biometricsNo == "'+biometrics_no+'")]');
-			} else {
-				data = await redis.json.get(key);
-			}
-		} else {
-			data = await redis.get(key);
-		}
+	    return accumulator;
+          }, [])
+	  : [];
+      } else if(last_name) {
+        data = await redis.json.get(key, '$.[?(@.lastName == "'+last_name+'")]');
+      } else if(first_name) {
+	data = await redis.json.get(key, '$.[?(@.firstName == "'+first_name+'")]');
+      } else if(employee_id) {
+	data = await redis.json.get(key, '$.[?(@.employeeID == "'+employee_id+'")]');
+      } else if(biometrics_no) {
+	data = await redis.json.get(key, '$.[?(@.biometricsNo == "'+biometrics_no+'")]');
+      } else {
+	data = await redis.json.get(key);
+      }
+    } else {
+      data = await redis.get(key);
+    }
 
-		// ! ----------------------------------------------------------------------------------------------------
+    // ! ----------------------------------------------------------------------------------------------------
 
-		console.timeEnd('Duration');
-		console.log('[End] '+key);
+    console.timeEnd('Duration');
+    console.log('[End] '+key);
 
     return new Response(JSON.stringify({
       [key]: data
